cf54a15cedbdd0ced1e8e4de0de9865c3243865a
kapritomi <90307599+kapritomi@users.noreply.github.com>
2022-10-03 19:52:50 +0200

    Add files via upload

--- suli_projekt/amoba.js
+++ suli_projekt/amoba.js
@@ -1,6 +1,7 @@
 let range = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let jatekos = 0;
 let gep = 0;
+let draw = 0;
 let steps = {
     1: "",
     2: "",
@@ -13,22 +14,18 @@ let steps = {
     9: ""
 }
 
-
 function reply_click(clicked_id) {
-    //console.log(range)
-//Játékos választása
+
     let human = Number(clicked_id);
-//Jétkos választásának kivétele a tömbből
+
     const index1 = range.indexOf(human);
     if (index1 > -1) {
         range.splice(index1, 1);
-//Funkció meghivása X-el
-    if(range.length !== 0 && check_winner() !== "jatekos" && check_winner() !== "gep"){
-        addSymbol(human);
-    }
-//Gép választásának generálása
+    
+        addSymbol(human);   
+    
         let pc = range[Math.floor(Math.random() * range.length)];
-//Gép választásának kivétele a tömbből
+    
         const index = range.indexOf(pc);
         if (index > -1) {
             range.splice(index, 1);
@@ -40,22 +37,62 @@ function reply_click(clicked_id) {
                 document.getElementById(player).innerHTML = "X";
                 document.getElementById(player).style.color = "#FFDB58";
                 steps[player] = "X";
-                check_winner();
             }
             else if(player == pc) {
                 document.getElementById(player).innerHTML = "O";
                 document.getElementById(player).style.color = "#FFA500";
                 steps[player] = "O";
-                check_winner();
             }
         }
-        
+        setTimeout(() => {
         if(range.length !== 0 && check_winner() !== "jatekos" && check_winner() !== "gep"){
-            setTimeout(() => {
-                addSymbol(pc)
-            }, "250")
+            addSymbol(pc)
+        }
+    }, "200")
+        tie();
+ 
+         if(range.length === 0 && check_winner() !== "jatekos" && check_winner() !== "gep"){
+            alert("döntetlen")
+            draw++;
+            document.getElementById("draw").innerHTML = draw;
+            reset();
         }
     }
+}
+function tie(){
+    let gyoztes = "";
+    setTimeout(() => {
+        if(steps[1] == "X" && steps[2] == "X"&& steps[3] == "X"||
+        steps[4] == "X" && steps[5] == "X" && steps[6] == "X"||
+        steps[7] == "X" && steps[8] == "X" && steps[9] == "X"||
+        steps[1] == "X" && steps[4] == "X" && steps[7] == "X"||
+        steps[2] == "X" && steps[5] == "X" && steps[8] == "X"||
+        steps[3] == "X" && steps[6] == "X" && steps[9] == "X"||
+        steps[1] == "X" && steps[5] == "X" && steps[9] == "X"||
+        steps[3] == "X" && steps[5] == "X" && steps[7] == "X")
+        {
+            jatekos++;
+            document.getElementById("player").innerHTML = jatekos;
+            reset();
+            alert("A játékos nyert")
+            gyoztes += "jatekos1";
+        }
+        else if(steps[1] == "O" && steps[2] == "O" && steps[3] == "O"||
+        steps[4] == "O" && steps[5] == "O" && steps[6] == "O"||
+        steps[7] == "O" && steps[8] == "O" && steps[9] == "O"||
+        steps[1] == "O" && steps[4] == "O" && steps[7] == "O"||
+        steps[2] == "O" && steps[5] == "O" && steps[8] == "O"||
+        steps[3] == "O" && steps[6] == "O" && steps[9] == "O"||
+        steps[1] == "O" && steps[5] == "O" && steps[9] == "O"||
+        steps[3] == "O" && steps[5] == "O" && steps[7] == "O"){
+            gep++;
+            document.getElementById("pc").innerHTML = gep;
+            alert("A gép nyert");
+            reset();
+            gyoztes+= "gep1";
+        }
+    }, "250")
+    return gyoztes
 }
 
 let win = [
@@ -72,18 +109,9 @@ function check_winner(){
        
         if(equal){
             if(steps[win[i][0]] == "X"){
-    
-                jatekos++;
-                console.log(jatekos)
-                document.getElementById("player").innerHTML = jatekos; 
-                alert("játékos nyert")
                 winner+= "jatekos";
             }
             else if(steps[win[i][0]] == "O"){
-                
-                gep++;
-                document.getElementById("pc").innerHTML = gep;
-                alert("gép nyert");
                 winner += "gep";
             }
         }
@@ -94,14 +122,11 @@ function check_winner(){
 function allAreEqual(array) {
   const result = array.every(element => {
     if (steps[element] === steps[array[0]] && steps[element] != "") {
-        console.log("következő elem: "+ steps[element] + ", első elem: "+steps[array[0]])
       return true;
     }
   });
   return result;
 }
-
-
 
 function reset() {
     range = []
